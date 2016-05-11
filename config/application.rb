4fa232118a2041769a68f63cb2012ab0b3674186
MainShayne233 <shaynetremblay@hotmail.com>
2016-05-11 13:34:14 -0400

    removed precompile false var

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,5 @@ module FiveTee
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
   end
 end
